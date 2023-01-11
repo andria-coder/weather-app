d3d62c3f4ecf93434bcfa67e4eecb9f9fb5619c2
andria-coder <115628085+andria-coder@users.noreply.github.com>
2023-01-11 23:19:38 +0100

    Update script.js

--- src/script.js
+++ src/script.js
@@ -13,18 +13,22 @@ let day = days[now.getDay()];
 
 let hour = now.getHours();
 let minutes = now.getMinutes();
+if (minutes < 0) {
+  minutes = `0${minutes}`;
+}
 currentTime.innerHTML = `${day}, ${hour}:${minutes}`;
 
-function displayForecast() {
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
-  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday"];
-
   let forecastHTML = `<div class= "row">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (day) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
       <div class="col-2">
          <div class="day">${day}</div>
              <i class="fa-solid fa-sun"></i>
@@ -34,41 +38,43 @@ function displayForecast() {
                 </div>
       </div>
       `;
+    }
   });
-
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
 
-function searchCity(event) {
-  event.preventDefault();
-  let city = document.querySelector("#city");
-  let inputCity = document.querySelector("#input-city");
-  city.innerHTML = `${inputCity.value}`;
+function getForecast(coordinates) {
+  let apiKey = "701f06352d61835bc4fc894e7b084629";
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
+
+function showWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#current-temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+}
+
+function search(city) {
+  let apiKey = "701f06352d61835bc4fc894e7b084629";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  axios.get(`${apiUrl}`).then(showWeatherCondition);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#input-city").value;
+  search(city);
+}
+
 let form = document.querySelector("form");
-form.addEventListener("submit", searchCity);
+form.addEventListener("submit", handleSubmit);
 
-displayForecast();
-
-function showCelcius(event) {
-  event.preventDefault();
-  let currentHigh = document.querySelector("#current-high");
-  currentHigh.innerHTML = 17;
-  let currentLow = document.querySelector("#current-low");
-  currentLow.innerHTML = 10;
-}
-
-let celciusLink = document.querySelector("#celcius-link");
-celciusLink.addEventListener("click", showCelcius);
-
-function showFarenheight(event) {
-  event.preventDefault();
-  let currentHigh = document.querySelector("#current-high");
-  currentHigh.innerHTML = 62;
-  let currentLow = document.querySelector("#current-low");
-  currentLow.innerHTML = 50;
-}
-
-let farenheightLink = document.querySelector("#farenheight-link");
-farenheightLink.addEventListener("click", showFarenheight);
+displayForecast(response);
